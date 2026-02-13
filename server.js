241aa9a444d6d9586e2bd0d91b8d5b4a45221ef0
vineettiwari1708 <vineettiwari1708@email.com>
2026-02-13 16:22:34 +0530

    db connect

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config.js';
 import cors from 'cors';
 import http from 'http';
+import { connectDB } from './lib/db.js';
 
 // create express app and http server
 const app = express();
@@ -12,6 +13,9 @@ app.use(express.json({ limit: '4mb' }));
 app.use(cors());
 
 app.use('/api/status', (req, res) => res.send('Server is live'));
+
+//connect to data base 
+await connectDB();
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log('Server is running on PORT:' + PORT));
