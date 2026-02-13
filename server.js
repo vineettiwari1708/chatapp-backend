03c901cc5ee9117e7b9cc453cf50a58d187aa060
vineettiwari1708 <vineettiwari1708@email.com>
2026-02-13 15:51:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config.js';
+import cors from 'cors';
+import http from 'http';
+
+// create express app and http server
+const app = express();
+const server = http.createServer(app);
+
+//middleware setup
+app.use(express.json({ limit: '4mb' }));
+app.use(cors());
+
+app.use('/api/status', (req, res) => res.send('Server is live'));
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log('Server is running on PORT:' + PORT));
